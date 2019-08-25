ed4a0099d9bdd673810c4e09607cfe87bbdb78b9
Yonatan Rojas S <yonatan@bedu.org>
2019-08-25 16:01:18 -0500

    feat: consumed mock data and added emmail and username props

--- Sesion05-manejo-de-estado-componentes-funcionales/post-work/mi-aplicacion/src/App.js
+++ Sesion05-manejo-de-estado-componentes-funcionales/post-work/mi-aplicacion/src/App.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 import './App.css'
 import Character from './Character'
-
-const URL = 'https://rickandmortyapi.com/api/character/'
+import fakeData from './db/data.json'
 
 function App(props) {
   const [data, setData] = React.useState([])
 
   React.useEffect(() => {
-    fetch(URL)
-    .then(r => r.json())
-    .then(r => setData(r.results))
+    // fetch(URL)
+    // .then(r => r.json())
+    // .then(r => setData(r.results))
+    setData(fakeData)
   }, [])
 
   return (
@@ -22,9 +22,10 @@ function App(props) {
             key={d.id}
             image={d.image}
             name={d.name}
-            specie={d.species}
+            username={d.username}
+            email={d.email}
           />
-        )
+        );
       })}
     </div>
   )
